40d6ef59e5afd78f3fb7613036c744046325968e
FelipeSulser <felipesulser@gmail.com>
2015-03-28 18:20:34 +0100

    Initial commit

---
+++ JPA-SSC/src/jpa/ssc/Ciudadano.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.ssc;
+
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
+import javax.persistence.*;
+
+
+
+/**
+ *
+ * @author felipesulser
+ */
+@Entity
+@Table(uniqueConstraints={@UniqueConstraint(columnNames={"dni"})})
+public class Ciudadano implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+   
+    private String dni;
+    
+    
+    private String nombre;
+
+    private String apellido1;
+    private String apellido2;
+    private String nacionalidad;
+    private Date fecha_nacimiento;
+    
+    
+    @OneToMany(mappedBy="ciudadano")
+    private List<Cita> citas;
+    
+    
+    
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ciudadano)) {
+            return false;
+        }
+        Ciudadano other = (Ciudadano) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "jpa.sercsocial.Ciudadano[ id=" + id + " ]";
+    }
+    
+}
