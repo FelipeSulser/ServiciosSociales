5056200a315d70e4a89b09958f1ba2195bf7676e
FelipeSulser <felipesulser@gmail.com>
2015-04-22 16:34:35 +0200

    Inicializado el nuevo proyecto J2E

---
+++ SSC_JSF/SSC_JSF-ejb/src/java/jpa/ssc/Cita.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.ssc;
+
+import java.io.Serializable;
+import java.sql.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author Grupo E
+ */
+@Entity
+public class Cita implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(nullable=false)
+    private Date fecha;
+    
+    
+    private String comentarios;
+
+    @Column(nullable=false)
+    private String tipo_de_cita;
+
+
+    
+    @ManyToOne
+    @JoinColumn(nullable=false)
+    private Ciudadano ciudadano;
+
+    
+    @ManyToOne
+    @JoinColumn(nullable=false)
+    private Profesional profesional;
+    
+    
+    
+    public Cita(){
+        
+    }
+    
+    public Ciudadano getCiudadano() {
+        return ciudadano;
+    }
+
+    public void setCiudadano(Ciudadano ciudadano) {
+        this.ciudadano = ciudadano;
+    }
+
+    public Profesional getProfesional() {
+        return profesional;
+    }
+
+    public void setProfesional(Profesional profesional) {
+        this.profesional = profesional;
+    }
+    
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+    
+    public String getTipo_de_cita() {
+        return tipo_de_cita;
+    }
+
+    public void setTipo_de_cita(String tipo_de_cita) {
+        this.tipo_de_cita = tipo_de_cita;
+    }    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cita)) {
+            return false;
+        }
+        Cita other = (Cita) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "jpa.sercsocial.Cita[ id=" + id + " ]";
+    }
+    
+}
---
+++ SSC_JSF/SSC_JSF-ejb/src/java/jpa/ssc/Expediente.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.ssc;
+
+import java.io.Serializable;
+import java.util.List;
+import java.sql.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Transient;
+ 
+/**
+ *
+ * @author Grupo E
+ */
+@Entity
+public class Expediente implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    
+    @Column(nullable=false)
+    private String css;
+    @Column(nullable=false)
+    private String zona;
+    @Column(nullable=false)
+    private Date fecha_apertura;
+
+    @OneToMany(mappedBy = "propietario")
+    private List<Vivienda> viviendas;
+
+    @OneToOne(mappedBy="expediente_residencia")
+    private Vivienda residencia;
+    
+    @OneToMany(mappedBy = "expediente_fam")
+    private List<Familiar> familiares;
+    
+    @OneToMany(mappedBy="expediente")
+    private List<Intervenciones> intervenciones;
+    
+    @OneToOne
+    @JoinColumn(nullable=false)
+    private Ciudadano ciudadano_exp;
+    
+    public Vivienda getResidencia() {
+        return residencia;
+    }
+
+    public void setResidencia(Vivienda residencia) {
+        this.residencia = residencia;
+    }
+    
+    public List<Familiar> getFamiliares() {
+        return familiares;
+    }
+
+    public void setFamiliares(List<Familiar> familiares) {
+        this.familiares = familiares;
+    }
+
+    public List<Intervenciones> getIntervenciones() {
+        return intervenciones;
+    }
+
+    public void setIntervenciones(List<Intervenciones> intervenciones) {
+        this.intervenciones = intervenciones;
+    }
+    
+    public Expediente(){
+        
+    }
+    public String getCss() {
+        return css;
+    }
+
+    public void setCss(String css) {
+        this.css = css;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+    
+    
+
+    public List<Vivienda> getViviendas() {
+        return viviendas;
+    }
+
+    
+    public void setViviendas(List<Vivienda> viviendas) {
+        this.viviendas = viviendas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Date getFecha_apertura() {
+        return fecha_apertura;
+    }
+
+    public void setFecha_apertura(Date fecha_apertura) {
+        this.fecha_apertura = fecha_apertura;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Expediente)) {
+            return false;
+        }
+        Expediente other = (Expediente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "jpa.sercsocial.Expediente[ id=" + id + " ]";
+    }
+    
+}
